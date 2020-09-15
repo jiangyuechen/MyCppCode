1b09f0b3df118484a9252cc84d13dd3e38988803
J_Ignite <yhgdjcl@outlook.com>
2020-09-15 18:02:10 +0800

    9.15

--- Courseware/Luogu/7.29_2/消耗战.cpp
+++ Courseware/Luogu/7.29_2/消耗战.cpp
@@ -6,7 +6,8 @@ typedef long long ll;
 const int maxn = 5e5 + 5;
 const ll inf = 1ll << 50;
 
-struct edge{
+struct edge
+{
     int v, w;
 };
 vector<edge> adj[maxn];
@@ -25,36 +26,41 @@ void dfs(int u)
     for (int j = 1; j < 20; j++)
         par[u][j] = par[par[u][j - 1]][j - 1],
         minw[u][j] = min(minw[u][j - 1], minw[par[u][j - 1]][j - 1]);
-    for (auto e: adj[u])
-    if (e.v != par[u][0])
-        dep[e.v] = dep[u] + 1, par[e.v][0] = u, minw[e.v][0] = e.w, dfs(e.v);
+    for (auto e : adj[u])
+        if (e.v != par[u][0])
+            dep[e.v] = dep[u] + 1, par[e.v][0] = u, minw[e.v][0] = e.w, dfs(e.v);
 }
 
 int lca(int u, int v)
 {
-    if (dep[u] < dep[v]) swap(u, v);
+    if (dep[u] < dep[v])
+        swap(u, v);
     for (int d = dep[u] - dep[v], k = 0; d; d >>= 1, ++k)
-        if (d & 1) u = par[u][k];
-    if (u == v) return u;
+        if (d & 1)
+            u = par[u][k];
+    if (u == v)
+        return u;
     for (int j = 19; j >= 0; j--)
-    if (par[u][j] != par[v][j])
-        u = par[u][j], v = par[v][j];
+        if (par[u][j] != par[v][j])
+            u = par[u][j], v = par[v][j];
     return par[u][0];
 }
 int minedge(int u, int v) //返回 u 到 祖先 v 路径上的最小边
 {
     int ans = 1 << 30;
     for (int d = dep[u] - dep[v], k = 0; d; d >>= 1, ++k)
-        if (d & 1) ans = min(ans, minw[u][k]), u = par[u][k];
+        if (d & 1)
+            ans = min(ans, minw[u][k]), u = par[u][k];
     return ans;
 }
-bool cmp(int x, int y) {return dfn[x] < dfn[y];}
+bool cmp(int x, int y) { return dfn[x] < dfn[y]; }
 
 void solve(int u)
 {
     dp[u][0] = (flag[u] == tim ? inf : 0);
     dp[u][1] = 0;
-    for (auto e : tmp[u]) {
+    for (auto e : tmp[u])
+    {
         solve(e.v);
         if (dp[u][0] != inf)
             dp[u][0] += min(dp[e.v][0], dp[e.v][1] + e.w);
@@ -66,24 +72,27 @@ int main()
     scanf("%d", &n);
     for (int u, v, w, i = 1; i < n; i++)
         scanf("%d %d %d", &u, &v, &w),
-        adj[u].push_back((edge){v, w}),
-        adj[v].push_back((edge){u, w});
-    
+            adj[u].push_back((edge){v, w}),
+            adj[v].push_back((edge){u, w});
+
     dfs(1);
 
     scanf("%d", &m);
-    while (m--) {
+    while (m--)
+    {
         /* 清零上一次的数组 */
-        for (int i = 1; i <= k; i++) tmp[seq[i]].clear();
+        for (int i = 1; i <= k; i++)
+            tmp[seq[i]].clear();
 
-        scanf("%d", &k); ++tim; //全局标记
+        scanf("%d", &k);
+        ++tim; //全局标记
         for (int i = 1; i <= k; i++)
             scanf("%d", &seq[i]), flag[seq[i]] = tim;
         sort(seq + 1, seq + k + 1, cmp); // 按照dfs序排序
-        for (int i = k; i > 1; i--) //之所以倒着写，是为了省事
+        for (int i = k; i > 1; i--)      //之所以倒着写，是为了省事
             seq[++k] = lca(seq[i], seq[i - 1]);
-        seq[++k] = 1; // 确保根在虚树中
-        sort(seq + 1, seq + k + 1, cmp); // 按照dfs序排序
+        seq[++k] = 1;                               // 确保根在虚树中
+        sort(seq + 1, seq + k + 1, cmp);            // 按照dfs序排序
         k = unique(seq + 1, seq + k + 1) - seq - 1; // 去重
         for (int i = 2; i <= k; i++)
         {
---
+++ P1026.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define END_PROGRAM  \
+    system("pause"); \
+    return 0;
+using namespace std;
+constexpr int MAXN = 205;
+
+string str;
+int p, k;
+int f[MAXN];
+
+void init()
+{
+    cin >> p >> k;
+    for (int i = 1; i <= p; i++)
+    {
+        string tmp;
+        cin >> tmp;
+        str += tmp;
+    }
+}
+
+int main()
+{
+
+    END_PROGRAM
+}
