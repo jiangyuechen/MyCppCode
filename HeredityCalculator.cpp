bb0dbfb1231e85df2186edef045cf8d83743fce8
岳宸 姜 <yhgdjcl@outlook.com>
2023-07-26 10:24:43 +0800

    2023.7.26

--- HeredityCalculator.cpp
+++ HeredityCalculator.cpp
@@ -4,31 +4,41 @@ using namespace std;
 
 class Bio;
 class Chromosome;
-class ChromosomeSet;     // 染色体组
-class SexualChromosome;  // 性染色体
-class Autosome;          // 常染色体
+class ChromosomeSet;    // 染色体组
+class SexualChromosome; // 性染色体
+class Autosome;         // 常染色体
 
-class Bio {
-   private:
-    vector<Chromosome> chrset;
+class Bio
+{
+private:
+   vector<Chromosome> chrset;
 
-   public:
+public:
 };
 
-class AsexualBio;  // 无性别分化的生物
-class SexualBio;   // 有性别分化的生物
+class AsexualBio; // 无性别分化的生物
+class SexualBio;  // 有性别分化的生物
 
-class Chromosome {};
-class SexualChromosome : public Chromosome {};
-class Autosome : public Chromosome {};
-
-class Gene {
-   protected:
-    string name;
-
-   public:
+class Chromosome
+{
+};
+class SexualChromosome : public Chromosome
+{
+};
+class Autosome : public Chromosome
+{
 };
 
-class Trait {};
+class Gene
+{
+protected:
+   string name;
+
+public:
+};
+
+class Trait
+{
+};
 
 int main() {}
--- JavaScriptArea/1.ts
+++
@@ -1,9 +0,0 @@
-var Tolerance = 0;
-var AndOr = 0;
-const And = 0, Or = 1;
-
-
-function SetKeyword (...kwd: string[]) {}
-function SetIgnored (...ign: string[]) {}
-function AddKeyword (...addKwd: string[]) {}
-function AddIgnored (...addIgn: string[]) {}
